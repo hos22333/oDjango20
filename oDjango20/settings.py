4c001a352374726b92c0c763c722cf28adefbdef
U1 <U1@LAPTOP-701UTDT6>
2024-06-20 02:53:20 +0300

    oV0620_0200

--- oDjango20/settings.py
+++ oDjango20/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c84fdfbd-73b8-4479-a3ae-423f3cb37ed3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
